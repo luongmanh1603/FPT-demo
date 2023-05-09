e96be7afcf034f600579850d87317d2443a86d35
luongmanh1604 <114908089+luongmanh1604@users.noreply.github.com>
2023-05-09 15:10:18 +0700

    slot 9-10

---
+++ src/demo4/Human.java
@@ -0,0 +1,14 @@
+package demo4;
+
+public abstract class Human {
+      public String name;
+      public int age;
+      public String address;
+
+      // abstract method
+      public abstract void eat();
+      public abstract void walk();
+
+
+      // khong the tao 1 doi tuong tu lop abstract
+}
