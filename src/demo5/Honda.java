5ea7566c6046964fb8139981a4501a8ccc1d346d
luongmanh1604 <114908089+luongmanh1604@users.noreply.github.com>
2023-05-09 15:10:15 +0700

    slot 9-10

---
+++ src/demo5/Honda.java
@@ -0,0 +1,14 @@
+package demo5;
+
+import demo4.Car;
+
+public class Honda extends Car {
+
+    public void run() {
+        this.setBrand("Honda");
+    }
+    public void Addprice() {
+        this.setPrice(1000);
+    }
+
+}
