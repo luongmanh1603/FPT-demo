b8c64606654365850782b6c7802ddec95453083a
luongmanh1604 <114908089+luongmanh1604@users.noreply.github.com>
2023-05-16 17:02:37 +0700

    TEXT

--- src/javax/HomeController.java
+++ src/javax/HomeController.java
@@ -1,8 +1,11 @@
 package javax;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+
+import java.util.ArrayList;
 
 public class HomeController {
 
@@ -10,12 +13,28 @@ public class HomeController {
     public TextField txtEmail;
     public TextField txtPhone;
     public Text txtOut;
+    public ArrayList<String> emailList = new ArrayList<>();
+    public ArrayList<String> phoneList = new ArrayList<>();
 
     public void submit(ActionEvent actionEvent) {
         String name = txtName.getText();
         String email = txtEmail.getText();
         String phone = txtPhone.getText();
-        txtOut.setText(name+"\n"+email+"\n"+phone);
+        try {
+            if (emailList.contains(email))
+                throw new Exception("Email da ton tai");
+            if (phoneList.contains(phone))
+                throw new Exception("Phone da ton tai");
+            emailList.add(email);
+            phoneList.add(phone);
+            String sv = name + "\n" + email + "\n" + phone;
+            txtOut.setText(txtOut.getText() + sv);
+        } catch (Exception e){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText(e.getMessage());
+            alert.show();
+        }
+
 
     }
 }
